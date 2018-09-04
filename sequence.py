ce1b338d0a3c766d1eedde6c06eaeb22a7dc11d8
Fridrik <fridrikj18@ru.is>
2018-09-04 15:50:38 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -1,15 +1,28 @@
-#Finna ut hvernig thetta virkar
+
 #1, 2, 3, 6, 11, 20, 37, 68, 125, 230
-#
+
+
+
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-num_1 = 1
-num_2 = 2
-num_3 = 3
-count = 0
+num_1 = 0
+num_2 = 0
+num_3 = 1
+sum = 0
+
 for i in range (0, n):
-    num_4 = num_1 + num_2 + num_3
-    num_5 = num_2 + num_3 + num_4
-    count += i
-    print (n)
+    if i < 3:
+        sum = num_2 + num_3
+        print(sum)
+        num_1 = num_2
+        num_2 = num_3
+        num_3 = sum
+    
+    else:
+        sum = num_1 + num_2 + num_3
+        print (sum)
+        num_1 = num_2 
+        num_2 = num_3
+        num_3 = sum
+
 
 
