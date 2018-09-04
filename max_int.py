6d22cc10b38de8659fea31fb203681fc686883a4
Fridrik <fridrikj18@ru.is>
2018-09-04 14:17:23 +0000

    Fyrstatilraun

--- max_int.py
+++ max_int.py
@@ -1,2 +1,10 @@
 #Assignment 5: Algorithms and git
+#
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+while num_int >= 0:
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
 
