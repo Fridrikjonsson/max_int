143381cf31a2ab70c7eb08d397f365f21c31d06b
Fridrik <fridrikj18@ru.is>
2018-09-04 14:56:10 +0000

    i vinnslu

--- sequence.py
+++ sequence.py
@@ -1,3 +1,15 @@
 #Finna ut hvernig thetta virkar
 #1, 2, 3, 6, 11, 20, 37, 68, 125, 230
 #
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+num_1 = 1
+num_2 = 2
+num_3 = 3
+count = 0
+for i in range (0, n):
+    num_4 = num_1 + num_2 + num_3
+    num_5 = num_2 + num_3 + num_4
+    count += i
+    print (n)
+
+
